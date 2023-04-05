00496bbd222adb79c4a88da71168d5de6d5b07fb
Mateusz Szymanski <mateusz.szymanski93@gmail.com>
2023-04-05 18:11:14 +0200

    set new code update

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const BUTTONC = document.querySelector(".numbers__btn--c")
 const DISPLAY = document.querySelector(".display")
 const WIN = document.querySelector(".win")
 const LOOSE = document.querySelector(".loose")
+const SETTINGS = document.querySelector(".settings")
+const OPEN_SETTINGS = document.querySelector(".btn-settings")
+const CLOSE_SETTINGS = document.querySelector(".settings__close")
+const NEW_CODE_INFO = document.querySelector(".newcode__info")
+const PASTE_NEW_CODE = document.querySelector(".info__code")
 
 let displayArr = []
 let currentCode = "62#2633"
@@ -29,19 +34,19 @@ BUT.forEach((button) =>
 
 function clickMusic()
 {
-    let audio = new Audio("click.mp3");
+    let audio = new Audio("voice/click.mp3");
     audio.play()
 }
 
 function failmusic()
 {
-    let audio = new Audio("fail.mp3");
+    let audio = new Audio("voice/fail.mp3");
     audio.play()
 }
 
 function openMusic()
 {
-    let audio = new Audio("open.mp3");
+    let audio = new Audio("voice/open.mp3");
     audio.play()
 }
 
@@ -83,18 +88,34 @@ function displayValue()
     }
 }
 
-const SUB_NEW_CODE = document.getElementById("sub-newcode")
+const SUB_NEW_CODE = document.querySelector(".newcode__sub-newcode")
 SUB_NEW_CODE.addEventListener("click", function()
 {
-    let firstDigits = document.getElementById("first-digits").value
-    let lastDigits = document.getElementById("last-digits").value
+    let firstDigits = document.querySelector(".newcode__digits--first").value
+    let lastDigits = document.querySelector(".newcode__digits--last").value
     currentCode = firstDigits + "#" + lastDigits
-    let ccLength = currentCode.length
-    console.log(ccLength)
-    console.log(currentCode)
+    NEW_CODE_INFO.classList.remove("hidden")
+    PASTE_NEW_CODE.innerText = currentCode
     return currentCode
 });
 
+OPEN_SETTINGS.addEventListener("click", function()
+{
+    SETTINGS.classList.remove("hidden")
+    SETTINGS.classList.add("fadein")
+    setTimeout(() => {
+        SETTINGS.classList.remove("fadein")
+    }, 800);
+})
 
+CLOSE_SETTINGS.addEventListener("click", function()
+{
+    SETTINGS.classList.add("fadeout")
+    setTimeout(() => {
+        SETTINGS.classList.add("hidden")
+        SETTINGS.classList.remove("fadeout")
+        NEW_CODE_INFO.classList.add("hidden")
+    }, 800);
+})
 
 
